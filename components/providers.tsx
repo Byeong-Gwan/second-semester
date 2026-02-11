198c4b916dc1791e5146709f928593b26b290dca
김병관 <ansd_my3073@naver.com>
2026-02-11 09:17:40 +0900

    feat: 수정

--- components/providers.tsx
+++ components/providers.tsx
@@ -6,7 +6,7 @@ import { ErrorBoundary } from "@/components/error-boundary";
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
       <ErrorBoundary>
         <ToastProvider>
           {children}
